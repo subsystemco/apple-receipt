faedfa20161e125b69e84e30e41ca1486ff9403a
Greg Leppert <greg@leppert.me>
2016-11-01 17:10:53 -0400

    Add private repo

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject apple-receipt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Utilities for working with receipts returned by Apple's StoreKit APIs and verifyReceipt endpoints."
+  :url "https://github.com/leppert/apple-receipt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[s3-wagon-private "1.2.0"]]
+
+  :repositories {"snapshots" {:url "s3p://libs.subsystem.co/snapshots"
+                              :username :env/aws_libs_access_key
+                              :passphrase :env/aws_libs_secret_key}
+                 "releases" {:url "s3p://libs.subsystem.co/releases"
+                             :username :env/aws_libs_access_key
+                             :passphrase :env/aws_libs_secret_key}})
